fe0f5eaadea23e71330b7386b9e6609f29560dbb
Viktoria562 <80815356+Viktoria562@users.noreply.github.com>
2021-03-17 10:24:23 +0200

    Add files via upload

---
+++ res/x5settings.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+|-------------------------------
+|	GENERAL SETTINGS
+|-------------------------------
+*/
+
+$imSettings['general'] = array(
+	'url' => 'http://websait.com',
+	'public_folder' => '',
+	'salt' => 'fcxunp0h2acv0p0xpf3yys6cyzmf2l4gk293517f'
+);
+
+// End of file x5settings.php
